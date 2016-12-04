3b6e4b89e0d62b0a388a18bdab726266a02e757d
David G Chaves <davidgchaves@gmail.com>
2016-12-04 20:46:59 +0100

    Add header and footer to BingoBuzz

--- Bingo.elm
+++ Bingo.elm
@@ -1,7 +1,42 @@
 module Bingo exposing (..)
 
 import Html
+import Html.Attributes
+
+
+playerInfo name gameNumber =
+    name ++ " - Game #" ++ (toString gameNumber)
+
+
+viewPlayer name gameNumber =
+    Html.h2
+        [ Html.Attributes.id "info"
+        , Html.Attributes.class "classy"
+        ]
+        [ Html.text (playerInfo name gameNumber) ]
+
+
+viewHeader title =
+    Html.header []
+        [ Html.h1 [] [ Html.text title ] ]
+
+
+viewFooter =
+    Html.footer []
+        [ Html.a
+            [ Html.Attributes.href "http://elm-lang.org" ]
+            [ Html.text "Powered By Elm" ]
+        ]
+
+
+view =
+    Html.div
+        [ Html.Attributes.class "content" ]
+        [ viewHeader "BUZZWORD BINGO"
+        , viewPlayer "David" 3
+        , viewFooter
+        ]
 
 
 main =
-    Html.text "Hello, World!"
+    view
